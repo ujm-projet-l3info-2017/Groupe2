d3be9df6f209aabcabdf29c1cfddabb4425456b3
Balto <balto@lain>
2017-03-06 18:29:54 +0100

    Changed from old url pattern container to the new one: a regular list.

--- GardenManager/GardenManager/urls.py
+++ GardenManager/GardenManager/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'GardenManager.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+  # Examples:
+  # url(r'^$', 'GardenManager.views.home', name='home'),
+  # url(r'^blog/', include('blog.urls')),
+  url(r'^admin/', include(admin.site.urls)),
+]
