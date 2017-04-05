cf5c4a01a8188d0bd81d8228cc52026b31f64171
Balto <balto@lain>
2017-04-05 14:48:39 +0200

    refactor(js/html): js separation
    Separation of the page structure and its behaviour.

---
+++ GardenManager/main/static/js/layout.js
@@ -0,0 +1,30 @@
+
+window.onload = function () {
+
+  document.getElementById ("global_info_alert").onclick = function () {
+    $("#flash_notice").hide () ;
+  } ;
+
+  document.getElementById ("submit_login_form_button").onclick = function () {
+    $('#submit_login')[0].click () ;
+  } ;
+
+  document.getElementById ("submit_register_form_button").onclick = function () {
+    $('#submit_register')[0].click () ;
+  } ;
+
+  document.getElementById ("register_user_password").onchange = function () {
+    $('#register_user_password_verif')[0].pattern = '^' + this.value + '$' ;
+  } ;
+
+  document.getElementById ("register_user_password_verif").onchange = function () {
+    $('#formRegister').validate () ;
+  } ;
+  document.getElementById ("register_user_password_verif").oninput = function () {
+    this.setCustomValidity('') ;
+  } ;
+  document.getElementById ("register_user_password_verif").oninvalid = function () {
+    this.setCustomValidity('Password does not match') ;
+  } ;
+
+} ;
