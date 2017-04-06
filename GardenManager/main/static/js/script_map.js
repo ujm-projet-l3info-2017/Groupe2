5ae0017fd5ee536c3a8e800a3076d89b5b4103c3
Balto <balto@lain>
2017-04-07 01:30:10 +0200

    Refactor(js): script_map.js
    JS code does not defined the function to get the google api key anymore and has been epurated.

--- GardenManager/main/static/js/script_map.js
+++ GardenManager/main/static/js/script_map.js
@@ -2,12 +2,7 @@
 
 var map ;
 
-
-getKey = function getKey () {
-  return "AIzaSyCt2Sw46PUXm2hF_e4yrztN0ogH44yeTFk" ;
-} ;
-
-function takePicture (document, map) {
+function takePicture () {
   var currentPosition = map.getCenter ();
   document.location.href =
     'https://maps.googleapis.com/maps/api/staticmap?' +
@@ -15,20 +10,10 @@ function takePicture (document, map) {
     '&center=' + currentPosition.lat () + ',' + currentPosition.lng () +
     '&zoom=' + map.getZoom () +
     '&size=640x400' +
-    '&key=' + getKey ();
-  /*
-   * document.location.href='https://maps.googleapis.com/maps/api/staticmap?' +
-   * 'maptype=hybrid?' +
-   * 'sensor=false&center=' +
-   * currentPosition.lat() + "," + currentPosition.lng() +
-   * "&zoom=" + map.getZoom()+"&key=" + getKey() + "&size=600x400&" +
-   * currentPosition.lat() + ',' + currentPosition.lng(); 
-   */
+    '&key=' + get_google_api_key () ;
 } ;
 
 function initialize () {
-  // var lat = google.loader.ClientLocation.latitude;
-  // var lng = google.loader.ClientLocation.longitude;
   var latlng = new google.maps.LatLng (46.227636, 2.213749);
   var options = {
     streetViewControl: false,
@@ -36,8 +21,9 @@ function initialize () {
     zoom: 5,
     mapTypeId: google.maps.MapTypeId.HYBRID
   } ;
-  map = new google.maps.Map (document.getElementById ("divMap"), options);
+  map = new google.maps.Map (document.getElementById ("google_map_api_div"), options);
 } ;
 
-window.initMap = function () {
-} ;
+window.addEventListener ("load", function () {
+  document.getElementById ("creation_plan_take_shot_button").onclick = takePicture
+}, false) ;
