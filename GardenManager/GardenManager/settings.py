830a430888850ae4451885cef6512231228778bd
Balto <balto@lain>
2017-04-12 03:36:47 +0200

    feat(configuration): ssl
    Make the server use https.

--- GardenManager/GardenManager/settings.py
+++ GardenManager/GardenManager/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'djangosecure',
+    'sslserver', 
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE=True
