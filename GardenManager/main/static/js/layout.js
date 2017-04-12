0566dc02a29db629329e22739c6e2970755f5a68
Balto <balto@lain>
2017-04-12 03:44:09 +0200

    refactor(js): layout (+variables)
    Refactoring the layout js file to use variables not to throw errors for absent html objecrs.

--- GardenManager/main/static/js/layout.js
+++ GardenManager/main/static/js/layout.js
@@ -2,31 +2,45 @@
 window.addEventListener ("load", function () {
 
   var alert = document.getElementById ("global_info_alert") ;
+  var submit_login_form_button = document.getElementById ("submit_login_form_button") ;
+  var submit_register_form_button = document.getElementById ("submit_register_form_button") ;
+  var register_user_password = document.getElementById ("register_user_password") ;
+  var register_user_password_verif = document.getElementById ("register_user_password_verif") ;
+
+
   if (alert !== null)
-    document.getElementById ("global_info_alert").onclick = function () {
+    alert.onclick = function () {
       $("#flash_notice").hide () ;
     } ;
 
-  document.getElementById ("submit_login_form_button").onclick = function () {
-    $('#submit_login')[0].click () ;
-  } ;
+  if (submit_login_form_button !== null){
+    submit_login_form_button.onclick = function () {
+      $('#submit_login')[0].click () ;
+    } ;
+  }
 
-  document.getElementById ("submit_register_form_button").onclick = function () {
-    $('#submit_register')[0].click () ;
-  } ;
+  if (submit_register_form_button !== null) {
+    submit_register_form_button.onclick = function () {
+      $('#submit_register')[0].click () ;
+    } ;
+  }
 
-  document.getElementById ("register_user_password").onchange = function () {
-    $('#register_user_password_verif')[0].pattern = '^' + this.value + '$' ;
-  } ;
+  if (register_user_password !== null) {
+    register_user_password.onchange = function () {
+      $('#register_user_password_verif')[0].pattern = '^' + this.value + '$' ;
+    } ;
+  }
 
-  document.getElementById ("register_user_password_verif").onchange = function () {
-    $('#formRegister').validate () ;
-  } ;
-  document.getElementById ("register_user_password_verif").oninput = function () {
-    this.setCustomValidity('') ;
-  } ;
-  document.getElementById ("register_user_password_verif").oninvalid = function () {
-    this.setCustomValidity('Password does not match') ;
-  } ;
+  if (register_user_password_verif !== null) {
+    register_user_password_verif.onchange = function () {
+      $('#formRegister').validate () ;
+    } ;
+    register_user_password_verif.oninput = function () {
+      this.setCustomValidity('') ;
+    } ;
+    register_user_password_verif.oninvalid = function () {
+      this.setCustomValidity('Password does not match') ;
+    } ;
+  }
 
 }) ;
