09744601fc0838906c5666a8f6632c84d6d6f727
Balto <balto@lain>
2017-04-12 03:49:12 +0200

    feat(google map api): add img + flexibility
    add button to put photo of the map into img markup, and re select the img in case of error.

--- GardenManager/main/static/js/script_map.js
+++ GardenManager/main/static/js/script_map.js
@@ -2,15 +2,30 @@
 
 var map ;
 
+function toggle_draw_mode (id) {
+  if (arguments.length === 0) {
+    toggle_draw_mode ("google_map_api_div_wrapper") ;
+    toggle_draw_mode ("canvas-wrap") ;
+  } else {
+    element = document.getElementById (id) ;
+    style = element.style["display"] == "inline" ? "none" : "inline" ;
+    console.log (" make " + id + " " + style)
+    while (element.style["display"] !== style) {
+      element.style["display"] = style ;
+    }
+  }
+}
+
 function takePicture () {
+  toggle_draw_mode () ;
   var currentPosition = map.getCenter ();
-  document.location.href =
-    'https://maps.googleapis.com/maps/api/staticmap?' +
+  document.getElementById ("google_map_image").src = 'https://maps.googleapis.com/maps/api/staticmap?' +
     'maptype=satellite' +
     '&center=' + currentPosition.lat () + ',' + currentPosition.lng () +
     '&zoom=' + map.getZoom () +
     '&size=640x400' +
     '&key=' + get_google_api_key () ;
+    
 } ;
 
 function initialize () {
@@ -22,6 +37,7 @@ function initialize () {
     mapTypeId: google.maps.MapTypeId.HYBRID
   } ;
   map = new google.maps.Map (document.getElementById ("google_map_api_div"), options);
+  toggle_draw_mode ("google_map_api_div_wrapper") ;
 } ;
 
 window.addEventListener ("load", function () {
