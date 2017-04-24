e4f3cffbfb7047ecb7f84b64aad47ddc7b14865a
Pelegrin-Romain <pelegrinromain@gmail.com>
2017-04-24 22:21:38 +0200

    feat(view.py): class Weather (init, send and retrieve data from the OWM API)

--- GardenManager/main/views.py
+++ GardenManager/main/views.py
@@ -5,6 +5,11 @@ import os
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from models import User
+from urllib2 import build_opener
+from urllib2 import Request
+from json import load
+
+
 
 
 
@@ -35,3 +40,38 @@ def register (request):
 def logout (request):
   request.session["_old_post"] = request.POST
   return HttpResponseRedirect ("/")
+
+class Weather ():
+  
+  def __init__ (self, latitude, longitude):
+    request_url = self.get_request_url(latitude, longitude)
+    json_output = self.get_request_output(request_url)
+    self.__dict__  = load(json_output)
+    print self.list[0]["humidity"]
+    
+  def get_owm_api_key (self):
+    return '2d5dd571dc377d57fd57c3b30cea7335'
+
+  def get_request_url (self, latitude, longitude):
+    return 'http://api.openweathermap.org/data/2.5/forecast/daily?' +\
+     'lat=' + str(longitude) +\
+     '&lon=' + str(latitude) +\
+     '&mode=json' +\
+     '&units=metric' +\
+     '&cnt=7' +\
+     '&appid=' + str(self.get_owm_api_key())
+
+  def get_request_output (self, url):
+    opener = build_opener()
+    request = Request(url)
+    response = opener.open(request)
+    return response
+
+obj = Weather (45.41484, 4.398651)
+
+"""
+temperature :
+  min - max   -> 
+pluvio :
+prévenir si c'est violent
+"""
